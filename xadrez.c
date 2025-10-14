50d8d11d86e6a802cbcefd944cb32f96abe03c66
Wellington Kaleb <wellingtonkaleb.wk@gmail.com>
2025-10-14 18:34:13 +0000

    Tema 4 - Nivel Novato: Entrega do Desafio Novato.

--- xadrez.c
+++ xadrez.c
@@ -12,17 +12,16 @@ int main() {
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
     printf("Movimentos do Bispo::\n");
-
+    // Estrutura de Repeticao While para os movimentos do Bispo:
     while (bispo < 5) {
         printf("Bispo: 1 casa para a Cima-Direita (Casa:%d)\n", bispo + 1);
         bispo++;
     }
 
-
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
     printf("\nMovimentos da Torre:\n");
-
+    // Estrutura de Repeticao For para os movimentos da Torre:
     for (int torre = 0; torre < 5; torre++)
     {
         printf("Torre: 1 casa para a Direita (Casa:%d)\n", torre + 1);
@@ -31,7 +30,7 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
     printf("\nMovimentos da Rainha:\n");
-
+    // Estrutura de Repeticao Do-While para os movimentos da Rainha:
     do {
         printf("Rainha: 1 casa para Esquerda (Casa:%d)\n", rainha + 1);
         rainha++;
