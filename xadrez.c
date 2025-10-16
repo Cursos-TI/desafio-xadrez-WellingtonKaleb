7710f3a4e0b3f78afdb58f4d817db1e707967de7
Wellington Kaleb <wellingtonkaleb.wk@gmail.com>
2025-10-16 23:08:32 +0000

    Tema 4 - Nivel Mestre: Entrega do Desafio Mestre.

--- xadrez.c
+++ xadrez.c
@@ -4,54 +4,94 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+// Recursividade da Torre:
+void moverTorre()
+{
+
+    for (int torre = 0; torre < 5; torre++)
+    {
+
+        printf("Torre: 1 casa para Direita (Casa:%d)\n", torre + 1);
+
+    }
+
+}
+
+// Recursividade do Bispo:
+void moverBispo(int bispo)
+{
+    int bispoCima = 0, bispoDireita = 0;
+
+    while(bispoCima < 5)
+    {
+        printf("Bispo: 1 casa para Cima-");
+        bispoCima++;
+
+        while(bispoDireita < bispoCima)
+        {
+
+            printf("Direira (Casa:%d)\n", bispoCima);
+            bispoDireita++;
+        }
+    }
+
+}
+
+// Recursivida da Rainha:
+void moverRainha(int rainha)
+{
+
+    do
+    {
+
+        printf("Rainha: 1 casa para Esquerda (Casa:%d)\n", rainha + 1);
+        rainha++;
+
+    }
+    while(rainha < 8);
+
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
     int bispo = 0, rainha = 0;
 
+    
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
     printf("Movimentos do Bispo::\n");
     // Estrutura de Repeticao While para os movimentos do Bispo:
-    while (bispo < 5) {
-        printf("Bispo: 1 casa para a Cima-Direita (Casa:%d)\n", bispo + 1);
-        bispo++;
-    }
+    // Transformando a Estrutura de Repeticao do Bispo em Recursiva:
+    moverBispo(bispo);
+
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
     printf("\nMovimentos da Torre:\n");
     // Estrutura de Repeticao For para os movimentos da Torre:
-    for (int torre = 0; torre < 5; torre++)
-    {
-        printf("Torre: 1 casa para a Direita (Casa:%d)\n", torre + 1);
-    }
+    // Transformando a Estrutura de Repeticao da Torre em Recursiva:
+    moverTorre();
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
     printf("\nMovimentos da Rainha:\n");
     // Estrutura de Repeticao Do-While para os movimentos da Rainha:
-    do {
-        printf("Rainha: 1 casa para Esquerda (Casa:%d)\n", rainha + 1);
-        rainha++;
-    } while(rainha < 8);
+    // Transformando a Estrutura de Repeticao da Rainha em Recursiva:
+    moverRainha(rainha);
     
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+
     // Exibicao dos movimntos do Cavalo:
     printf("\nMovimentos do Cavalo\n");
 
     // Estrutura de Repeticao For com Do-While para os movimentos do Cavalo:
-    for (int cavalo = 1; cavalo >= 1; cavalo--)
+    // Aplicando Estrutura de loops Complexos no movimento do Cavalo:
+    for (int cavaloCima = 1, cavaloDireita = 1; cavaloCima >= cavaloDireita; cavaloDireita--)
     {
         do 
         {
 
             printf("Cavalo: 1 casa para Cima\n");
-            cavalo--;
+            cavaloCima--;
 
-        } while (cavalo >= 0);
+        } while (cavaloCima >= 0 ? 1 : 0);
 
         printf("Cavalo: 1 casa para a Direita\n");
     }
